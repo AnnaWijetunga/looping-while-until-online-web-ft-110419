87394efe896e06e42e0f28c071e2070444f478cd
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-06 13:39:36 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,10 +2,7 @@ def using_while
   levitation_force = 6
 	counter = 0 
 	while counter < 10 
-	  puts ""
-  #your code here
+	  puts "Wingardium Leviosa"
+	  counter += 1 
+	end 
 end
-
-# using_while puts out the phrase 'Wingardium Leviosa' while the levitationforce is incremented to 10
-# fill out the conttent to that calling it will puts the desired phrase while lev force is < 10.
-# every time you puts the phrase, increment by 1 
