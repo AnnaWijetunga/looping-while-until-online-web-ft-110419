a07b1fdf8892fbbe9e991a56459efa4cf674b0aa
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-06 13:44:18 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,9 +2,7 @@ def using_until
   levitation_force = 6
   counter = 6 
   until counter == 10 
-  puts ""
-    
+    puts "Wingardium Leviosa"
+    counter += 1 
+  end
 end
-
-#puts phrase until our lev force equal 10 
-#Every time you puts the phrase, increment by 1
